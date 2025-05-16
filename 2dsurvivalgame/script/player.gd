5e67a7d3d234cea40ee2366cf11dcfb451b8c99f
EishasGitHub <eishaetc@gmail.com>
2025-05-16 15:16:09 +0500

    [Video #6] Base setup & partial feature

--- 2dsurvivalgame/script/player.gd
+++ 2dsurvivalgame/script/player.gd
@@ -2,8 +2,12 @@ extends CharacterBody2D
 
 var speed = 100
 var player_state
+var bow_equipped = true
+var bow_cooldown = true
 
 @export var inv: Inv
+
+var arrow = preload("res://scene/arrow.tscn")
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("left", "right", "up", "down")
@@ -16,6 +20,18 @@ func _physics_process(delta: float) -> void:
 		
 	velocity = direction * speed
 	move_and_slide()
+	
+	var mouse_pos = get_global_mouse_position()
+	$Marker2D.look_at(mouse_pos)
+	
+	if (Input.is_action_just_pressed("left_mouse")) and (bow_equipped) and (bow_cooldown):
+		bow_cooldown = false
+		
+		var arrow_instance = arrow.instantiate()
+		arrow_instance.rotation = $'Marker2D'.rotation
+		arrow_instance.global_position = $Marker2D.global_position
+		
+		add_child(arrow_instance)
 	
 	play_anim(direction)
 	
